d71ba807095f526daf8a885b5dddee60d3573bd1
Luisão <98217717+Luisao-official@users.noreply.github.com>
2023-12-21 13:37:37 -0300

    Main script update
    - Bug fixes (list sorting)

--- OpenVSP/python/Genetic_scripts/Wing_Gen_Sim.py
+++ OpenVSP/python/Genetic_scripts/Wing_Gen_Sim.py
@@ -5,6 +5,7 @@ from time import perf_counter
 
 
 SECTION_LIMIT = 0.85
+SPAN_LIMIT = (0.085, 0.765)
 TIP_CHORD_LIMITS = (0.18, 0.2)
 SWEEP_LOC_LIMITS = (0, 0.4)
 MACH_SPEED = [0.029]
@@ -20,7 +21,7 @@ def initialize_population(n):
         # Generation random elements
         population.append(
             {
-                'span': round(random.uniform(0.085, 0.765), 2),
+                'span': round(random.uniform(SPAN_LIMIT[0], SPAN_LIMIT[1]), 2),
                 'tip_chord': round(random.uniform(TIP_CHORD_LIMITS[0], TIP_CHORD_LIMITS[1]), 2),
                 'sweep_loc': round(random.uniform(SWEEP_LOC_LIMITS[0], SWEEP_LOC_LIMITS[1]), 2)
             }
@@ -51,7 +52,7 @@ def mutate(element):
     }
 
     # Ensure parameters are within the specified ranges
-    mutated_element['span'] = max(min(mutated_element['span'], 0.765), 0.085)
+    mutated_element['span'] = max(min(mutated_element['span'], SPAN_LIMIT[1]), SPAN_LIMIT[0])
     mutated_element['tip_chord'] = max(min(mutated_element['tip_chord'], TIP_CHORD_LIMITS[1]), TIP_CHORD_LIMITS[0])
     mutated_element['sweep_loc'] = max(min(mutated_element['sweep_loc'], SWEEP_LOC_LIMITS[1]), SWEEP_LOC_LIMITS[0])
 
@@ -90,6 +91,7 @@ def get_score(element):
     vsp.SetDoubleAnalysisInput(analysis_name, "MachStart", MACH_SPEED, 0)
     vsp.SetDoubleAnalysisInput(analysis_name, "AlphaEnd", [1.00], 0)
 
+    # EExecuting the Analysis while logging the analysis time
     start_time = perf_counter()
     vsp.ExecAnalysis(analysis_name)
     end_time = perf_counter()
@@ -119,20 +121,22 @@ for iteration in range(ITERATOR_LIMITER):
 
     # Sort scores by score
     scores.sort(key=lambda x: x[0], reverse=True)
-    print(scores)
+    # print(scores)
 
     # Select the top-performing elements
     selected_elements = [elem for _, elem in scores[:(len(population)//2)]]
-    print(selected_elements)
+    # print(selected_elements)
 
     # Extract and store scores for the selected elements
     selected_scores = [score for score, _ in scores[:(len(population)//2)]]
-    print(selected_scores)
+    # print(selected_scores)
 
     best_scores = list(zip(selected_scores, selected_elements))
-    print(best_scores)
-    input()
+    # print(best_scores)
+    # input()
     if iteration == ITERATOR_LIMITER-1:
+        # skipping the natural loop end to avoid the list constructors below
+        # which will be slow when there are a lot of elements
         continue
 
     # Apply mutation to create new elements
@@ -141,7 +145,15 @@ for iteration in range(ITERATOR_LIMITER):
     # Replace worst-scoring elements
     population = selected_elements + new_elements
 
-
+if len(best_scores) <= TOP_N_SCORES:
+    while len(best_scores) != TOP_N_SCORES:
+        best_scores.append((0.000, {
+                'span': 0.000,
+                'tip_chord': 0.000,
+                'sweep_loc': 0.000
+            }))
+else:
+    best_scores = best_scores[:TOP_N_SCORES]
 
 # Extract parameters for the winner
 winner_params = [param for param in best_scores[0][1].values()]
@@ -150,7 +162,8 @@ wing_modifier(winner_params[0], winner_params[1], winner_params[2], True)
 file = open("results.txt", "w")
 # Print the top N parameters and scores
 for i, (score, params) in enumerate(best_scores):
-    print(f"Rank {i + 1}: Parameters {params} | Score {score}")
+    print(f"\nWing n{i+1}:\nscore: {score:.6e}\nspan: {params['span']:.4f}; tip_chord: {params['tip_chord']:.4f}; "
+          f"sweep_loc {params['sweep_loc']:.4f}\n")
     file.write(f"Rank {i + 1}:\n\nScore: {score}\nParameters: {params}\n\n\n")
 file.close()
 log.close()
